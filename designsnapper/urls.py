be2ec328fbc3d1eee5339d65c966b9a334fbda51
Jonathan <me@jonathan-kim.com>
2011-08-07 21:27:26 -0400

    Fixed invalid url struct.

--- designsnapper/urls.py
+++ designsnapper/urls.py
@@ -2,9 +2,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('designsnapper.views',
-    (r'^$', 'home', name='home'),
-    (r'settings', 'settings', name='settings'),
-    (r'pages', 'pages', name='pages'),
-    (r'page', 'page', name='page'),
-    (r'debug', 'debug', name='debug')
+    (r'^$', 'home'),
+    (r'settings', 'settings'),
+    (r'pages', 'pages'),
+    (r'page', 'page'),
+    (r'debug', 'debug')
 )
